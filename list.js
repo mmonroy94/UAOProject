bb966ad3967e0b5b1b63e50a66fe5b718de0b3fd
Melissa Monroy <melissamonroy@hotmail.com>
2023-10-27 00:55:03 -0300

    stock actualizado

---
+++ list.js
@@ -0,0 +1,183 @@
+let stock = [
+    {
+        id: 1,
+        name: 'Sniks pegasus',
+        price: 100000,
+        description: 'Zapatillas de running para carretera y mal clima',
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/3291fa27-8fc2-4ca5-9662-8192083045e1/streakfly-zapatillas-de-competicion-asfalto-FNPPmz.png',
+        cartQuantity: 0,
+        stock: 10
+    },
+    {
+        id: 2,
+        name: 'Sniks free',
+        price: 100000,
+        description: 'Zapatillas de running para asfalto',
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8219b64c-5fa5-46c1-83d4-57aee6aeffb7/renew-run-4-zapatillas-de-running-asfalto-4S6zsc.png',
+        cartQuantity: 0,
+        stock: 10
+    },
+    {
+        id: 3,
+        name: 'Sniks react',
+        price: 100000,
+        description: 'Zapatillas de running para asfalto',
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/02e63bb7-bb23-48d2-bd6c-4e1889369300/escape-run-2-zapatillas-de-running-asfalto-C9fmgC.png',
+        cartQuantity: 0,
+        stock: 10
+    },
+    {
+        id: 4,
+        name: 'Sniks phantom',
+        price: 100000,
+        description: 'Zapatillas de running para carretera y mal clima',
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/ab840d4d-c5d1-48c7-9e27-f990bccecef5/pegasus-shield-zapatillas-de-running-carretera-el-mal-tiempo-PWG1zl.png',
+        cartQuantity: 0,
+        stock: 10
+    },
+    {
+        id: 5,
+        name: 'Sniks zoomX',
+        price: 100000,
+        description: 'Zapatillas de competición para asfalto',
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a5ea1c4d-3eb8-47ec-9c5e-0568f3c93640/vaporfly-3-zapatillas-de-competicion-asfalto-VKkc7j.png',
+        cartQuantity: 0,
+        stock: 10
+    },
+    {
+        id: 6,
+        name: 'Sniks infinity trail',
+        price: 100000,
+        description: 'Zapatillas de trail running',
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/681005c1-0318-4ed3-b1b0-f8368283ec19/juniper-trail-2-next-nature-zapatillas-de-trail-running-sPRTRf.png',
+        cartQuantity: 0,
+        stock: 10
+    },
+    {
+        id: 7,
+        name: 'Sniks pegasus trail',
+        price: 100000,
+        description: 'Zapatillas de trail running impermeables',
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/20624bb9-9514-4e8f-97f9-d8885520a910/pegasus-trail-4-gore-tex-zapatillas-de-trail-running-el-mal-tiempo-tVWltb.png',
+        cartQuantity: 0,
+        stock: 10
+    },
+    {
+        id: 8,
+        name: 'Sniks pegasus trail',
+        price: 100000,
+        description: 'Zapatillas de trail running y mal clima',
+        img:'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e79725ad-02e9-408e-9ccb-3fccd8365acf/pegasus-trail-4-gore-tex-zapatillas-de-trail-running-el-mal-tiempo-49RhsL.png',
+        cartQuantity: 0,
+        stock: 10
+    },
+    {
+        id: 9,
+        name: 'Sniks juniper',
+        price: 100000,
+        description: 'Zapatillas de running para carretera impermeables',
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/03a6fb62-f216-4b77-8d41-85251786d182/juniper-trail-2-gore-tex-zapatillas-de-trail-running-impermeables-n5QMTj.png',
+        cartQuantity: 0,
+        stock: 10
+    },
+    {
+        id: 10,
+        name: 'Sniks infinity 10',
+        price: 100000,
+        description: 'Zapatillas de competición para carretera',
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/cb28e692-777a-4be3-97a5-b1b0406d4eef/alphafly-2-zapatillas-de-competicion-asfalto-fvDSdT.png',
+        cartQuantity: 0,
+        stock: 10
+    },
+    {
+        id: 11,
+        name: 'Sniks phoenix',
+        price: 100000,
+        description: 'Zapatillas de training',
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/uirypqqxq83mwcjbf9ej/air-monarch-iv-zapatillas-de-training-t6VLxN.png',
+        cartQuantity: 0,
+        stock: 10
+    },
+    {
+        id: 12,
+        name: 'Sniks take',
+        price: 100000,
+        description: 'Zapatillas de baloncesto',
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/2ade30d4-d24d-4955-b49e-b5d37b49e03d/lebron-witness-7-zapatillas-de-baloncesto-VrDzb7.png',
+        cartQuantity: 0,
+        stock: 10
+    },
+    {
+        id: 13,
+        name: 'Sniks zoom',
+        price: 100000,
+        description: 'Zapatillas para clases de HIIT',
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/caaa5b45-dce3-42a5-ba24-29f156bedc70/zoom-superrep-4-next-nature-zapatillas-clases-de-hiit-9dMT1S.png',
+        cartQuantity: 0,
+        stock: 10
+    },
+    {
+        id: 14,
+        name: 'Sniks dunk',
+        price: 100000,
+        description: 'Zapatillas para clases de HIIT',
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/cbf32590-72d3-49b4-a29f-590c2181aff4/air-zoom-superrep-3-zapatillas-clases-de-hiit-jXhdwS.png',
+        cartQuantity: 0,
+        stock: 10
+    },
+    {
+        id: 15,
+        name: 'Sniks precision',
+        price: 100000,
+        description: 'Zapatillas de baloncesto',
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/ed46f423-f38d-4ea9-b6c6-a70f7675c73f/gt-hustle-2-zapatillas-de-baloncesto-lXkKR7.png',
+        cartQuantity: 0,
+        stock: 10
+    },
+    {
+        id: 15,
+        name: 'Sniks roma',
+        price: 100000,
+        description: 'Zapatillas de halterofilia',
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/32a5ab47-35a7-41b6-a276-872ae96672e2/romaleos-4-se-zapatillas-de-halterofilia-QRPsN6.png',
+        cartQuantity: 0,
+        stock: 10
+    }
+]
+
+function addEvent(id) {
+    console.log('Se hizo click para agregar', id);
+    shoppingCart.addItem(id)
+}
+
+function deleteEvent(id) {
+    console.log('Se hizo click para eliminar', id);
+    shoppingCart.deleteItem(id)
+}
+
+function quantityEvent(object) {
+    console.log('Se hizo click para modificar cantidad');
+    shoppingCart.handleQuantity(object)
+}
+
+function showProducts () {
+    let cartProducts = ''
+    for (let i = 0; i < stock.length; i++) {
+        let product = `<div class="col-md-3">
+        <div class="card h-100" style="width: 18rem">
+          <img src="${stock[i].img}" class="card-img-top custom-img" alt="${stock[i].name}"/>
+          <div class="card-body">
+            <h5 class="card-title">${stock[i].name}</h5>
+            <p class="card-text">$ ${stock[i].price}</p>
+            <p class="card-text">${stock[i].description}</p>
+            <a href="#" class="btn btn-primary" onclick="addEvent(${stock[i].id});">Comprar</a>
+          </div>
+        </div>
+        </div>`
+        cartProducts += product
+    }
+    document.getElementById('storeProducts').innerHTML = cartProducts
+}
+
+showProducts()
+let shoppingCart = new CartItems(stock)
--- user/shoppingCart.js
+++ user/shoppingCart.js
@@ -1,26 +1,107 @@
 class CartItems {
     constructor(item) {
       this.item = item;
+      console.log('ESTE EL ITEM', this.item);
       this.selectedItems = [];
+      this.totalContainer = document.getElementById("shoppingCartTotal");
+      this.clearButton = document.getElementById('clearShoppingCart')
+      this.clearButton.addEventListener("click", () => {
+        this.clearCart()
+      });
+      this.errorMessage = ''
     }
   
     addItem(id) {
-      this.selectedItems.push(this.item[id]);
-      this.showItems();
+      let addedItem = this.selectedItems.find(item => item.id === id)
+      let newItem = this.item.find(newItem => newItem.id === id)
+
+      if(addedItem) {
+        addedItem.cartQuantity = addedItem.cartQuantity + 1
+
+        // MANEJO DE STOCK
+        if(addedItem.stock === 0){
+          alert(`El producto ${addedItem.name} no tiene más unidades disponibles`)
+        }else if(addedItem.stock > 0){
+          addedItem.stock = addedItem.stock - 1
+        }
+        this.showItems()
+
+        console.log('STOCK ORIGINAL', addedItem.stock);
+      }else{
+
+        newItem.cartQuantity = newItem.cartQuantity + 1
+        this.selectedItems.push(newItem);
+
+        // MANEJO DE STOCK
+        if(newItem.stock === 0){
+          alert(`Lo sentimos, el producto ${newItem.name} no tiene más unidades disponibles`)
+        }else if(newItem.stock > 0){
+          newItem.stock = newItem.stock - 1
+        }
+        this.showItems()
+        // console.log('STOCK ORIGINAL NUEVO PRODUCTO', newItem.stock);
+      }
+      // console.log('ITEMS SELECCIONADOS',this.selectedItems);
     }
   
     deleteItem(id) {
-      this.selectedItems.splice(id, 1);
+      // console.log('ID PARAMETRO DELETE', id)
+      // console.log('CANTIDAD PARAMETRO DELETE',cartQuantity);
+      let deletedItem = this.selectedItems.find(item => item.id === id)
+      // console.log('ITEM ELIMINADO', deletedItem);
+      deletedItem.stock = deletedItem.stock + deletedItem.cartQuantity
+      deletedItem.cartQuantity = 0
+      // console.log('STOCK RESTAURADO', deletedItem.stock);
+
+      const filteredItems = this.selectedItems.filter((element) => element.id !== id);
+      this.selectedItems = filteredItems;
+      
       this.showItems();
+      
+      // console.log('ACCION BORRAR',this.selectedItems);
     }
+
+    handleQuantity(object) {
+      const { type, id } = object
+      let product = this.item.find(newItem => newItem.id === id)
+      
+      if(product.stock >= 1){
+      if(type === 'add'){
+          product.cartQuantity = product.cartQuantity + 1
+          product.stock = product.stock - 1
+          
+        }
+      else if(type === 'subtract'){
+        if(product.cartQuantity > 1){
+          product.cartQuantity = product.cartQuantity - 1
+          product.stock = product.stock + 1
+        }
+      }
+      this.showItems()
+      console.log('CART QUANTITY', product.cartQuantity);
+      console.log('STOCK', product.stock);
+    }else if(product.stock === 0){
+      this.errorMessage = `Aviso: Inventario insuficiente`
+      this.showItems()
+    }
+      
+
+    }
+
+    clearCart(){
+      this.selectedItems.splice(0, this.selectedItems.length)
+      this.showItems()
+      console.log('STOCK FINAL', this.item);
+    }
+
     
     showItems() {
       let cartProducts = "";
       let total = 0;
-      let totalContainer = document.getElementById("shoppingCartTotal"); 
 
       for (let i = 0; i < this.selectedItems.length; i++) {
         let selectedItems = `<li class="list-group-item d-flex flex-row justify-content-around align-items-center">
+          
           <img
             src="${this.selectedItems[i].img}"
             class="item-img"
@@ -28,14 +109,19 @@ class CartItems {
           />
           <p>${this.selectedItems[i].name}</p>
           <p> $ ${this.selectedItems[i].price}</p>
-          <button type="button" class="btn btn-danger" onclick="deleteEvent(${i});"> X </button>
+          <p> ${this.selectedItems[i].cartQuantity}</p>
+    
+
+          <button type="button" class="btn btn-danger" onclick="quantityEvent({ type: 'add', id: ${this.selectedItems[i].id}});"> + </button>
+          <button type="button" class="btn btn-danger" onclick="quantityEvent({ type: 'subtract', id: ${this.selectedItems[i].id} });"> - </button>
+
+          <button type="button" class="btn btn-danger" onclick="deleteEvent(${this.selectedItems[i].id});"> X </button>
+          <p>${this.errorMessage}</p>
         </li>`;
         cartProducts += selectedItems;
-        total += this.selectedItems[i].price;
+        total += this.selectedItems[i].price * this.selectedItems[i].cartQuantity;
       }
       document.getElementById("shoppingCartList").innerHTML = cartProducts;
-      totalContainer.textContent = "Total: " + total; 
-// Agrega el elemento <p> como hijo del elemento con ID "shoppingCartTotal"
-
+      this.totalContainer.textContent = "Total: " + total; 
     }
   }
